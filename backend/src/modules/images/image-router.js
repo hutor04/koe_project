e1f13f834c696a3f824f05e510bf30d1b6bb2fb9
yauhen <yauhenk@uio.no>
2020-11-25 12:57:10 +0100

    DEV-07 Done

--- backend/src/modules/images/image-router.js
+++ backend/src/modules/images/image-router.js
@@ -1,17 +1,17 @@
 const express = require('express');
-const { MongoClient, ObjectId, GridFSBucket } = require('mongodb');
+const mongoose = require('mongoose');
+const { ObjectId, GridFSBucket } = require('mongodb');
 
-const client = new MongoClient('mongodb://localhost:27017/', { useUnifiedTopology: true });
 const router = express.Router();
 
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
-  await client.connect();
-  const db = client.db('b_good');
+  const { db } = mongoose.connection;
   const gfs = new GridFSBucket(db, { bucketName: 'gridfsdownload' });
   try {
     gfs.openDownloadStream(ObjectId(id)).pipe(res);
   } catch (e) {
+    // eslint-disable-next-line no-console
     console.log(e);
     res.end();
   }
--- frontend/src/components/VenueCard/VenueCard.js
+++ frontend/src/components/VenueCard/VenueCard.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Card, Image} from 'react-bootstrap';
-
+import defaultLogo from '../../imgs/default_logo.png';
 function VenueCard({ logo, name }) {
-  const logoPath = logo ? `images/logos/${logo}` : '';
+  const logoPath = logo ? `images/logos/${logo}` : defaultLogo;
   return (
     <Card className={'mt-2 flex-row flex-wrap'}>
-      <Image src={logoPath} style={{ width: "50px", height: "50px", margin: "5px" }} rounded />
+      <Image src={logoPath} style={{ height: "50px", margin: "5px" }} rounded />
       <Card.Body>{name}</Card.Body>
     </Card>
   );
